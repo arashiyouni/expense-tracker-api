e62fe06bc5469a022a430c2f5af37f2b3615307c
angie espinoza <angie.espinoza.campos@gmail.com>
2024-11-12 14:29:35 -0600

    added dependency auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
+import { TaskModule } from './modules/task/task.module';
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
+    UserModule,
+    TaskModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [AppService],
